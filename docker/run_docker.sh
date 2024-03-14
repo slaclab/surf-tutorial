f6bc4086ce3cedf58f9b5fa856fee60bd021402f
Larry Ruckman <ruckman@slac.stanford.edu>
2024-03-14 13:40:51 -0700

    update to support macOS X11 in docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Check OS using uname
+if [[ "$(uname)" == "Darwin" ]]; then
+   # macOS system detected
+   xhost + 127.0.0.1
+   DISPLAY='host.docker.internal:0'
+else
+   # Assuming Linux if not macOS
+   DISPLAY=${DISPLAY}
+fi
+
+# Start the docker
 docker run -ti \
    --net=host \
-   -e DISPLAY=${DISPLAY} \
+   -e DISPLAY=$DISPLAY \
    -v ${HOME}/.Xauthority:/home/${USER}/.Xauthority \
    -v /etc/localtime:/etc/localtime:ro \
    -v ${HOME}:/home/${USER} \
